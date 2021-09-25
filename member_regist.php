e5196bcac1b3656fc5dca1d2e4a7c0a35be7d3ed
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2021-09-25 20:21:40 +0900

    create db_table and member_confirm.php

--- member_regist.php
+++ member_regist.php
@@ -1,3 +1,24 @@
-<?php
-echo "phpディレクトリ作成しまた";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style>
+		.confirm{
+			color: white;
+			background-color: skyblue;
+			font-weight: bold;
+		}
+	</style>
+</head>
+<body>
+	<h1>会員情報登録フォーム</h1>
+	<form action="" method="POST">
+		<p>氏名</p>
+		姓<input type="text" name="name_sei" value="">
+		名<input type="text" name="name_mei" value=""><br><br>
+		<input class="confirm" type="submit" name="signup" value="確認画面へ">
+	</form>
+</body>
+</html>
