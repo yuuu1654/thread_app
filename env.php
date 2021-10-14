9a0b8a9b6cea99be9b1195d6e7722af07defff52
root <root@os3-382-24072.vs.sakura.ne.jp>
2021-10-15 00:24:03 +0900

    first commit

--- env.php
+++ env.php
@@ -1,15 +1,18 @@
 <?php
 	//DBの設定値を定義
-	// define("DB_HOST", "localhost");
-	// define("DB_NAME", "thread_app");
-	// define("DB_USER", "root");
-	// define("DB_PASS", "root");
+	define("DB_HOST", "localhost");
+	define("DB_NAME", "thread_app");
+	define("DB_USER", "root");
+	define("DB_PASS", "ekr33695");
 
 
-	define("DB_HOST", "us-cdbr-east-04.cleardb.com");
-	define("DB_NAME", "heroku_0229fc793cf8222");
-	define("DB_USER", "bfa09a5ab40e0e");
-	define("DB_PASS", "952510d5");
+
+
+	//herokuの環境変数
+	// define("DB_HOST", "us-cdbr-east-04.cleardb.com");
+	// define("DB_NAME", "heroku_0229fc793cf8222");
+	// define("DB_USER", "bfa09a5ab40e0e");
+	// define("DB_PASS", "952510d5");
 
 
 	//ユーザー名　bfa09a5ab40e0e
